1f1a4a4a1abb57e0f4536b000966e0e6c240f255
pejath <pejath@yandex.ru>
2023-08-27 23:17:32 +0300

    answers tests and model, updating questions

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   describe 'validations' do
-    # subject { FactoryBot.build(:question) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:body) }
+  end
+
+  describe 'relations' do
+    it { is_expected.to have_many(:answers) }
   end
 end
