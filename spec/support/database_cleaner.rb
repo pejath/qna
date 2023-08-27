8c0db8fbe147567efd6fc34b1fe29f5819a2be28
pejath <pejath@yandex.ru>
2023-08-27 23:05:44 +0300

    rspec setup

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,13 @@
+RSpec.configure do |config|
+  config.after(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+    FactoryBot.reload
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
