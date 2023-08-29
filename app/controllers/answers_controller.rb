2525354db4131ddc36d2551531566bb3d217bb3e
pejath <pejath@yandex.ru>
2023-08-29 21:18:40 +0300

    Answers controller and tests

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,20 @@
+class AnswersController < ApplicationController
+  expose :question
+  expose :answer
+
+  def create
+    @new_answer = question.answers.new(answer_params)
+
+    if @new_answer.save
+      redirect_to @new_answer.question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
