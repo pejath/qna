aa5ea13cb3a387d2d0c2b85d4f248db0a6d29589
pejath <pejath@yandex.ru>
2023-09-26 19:08:07 +0300

    rubocop run

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :question do
-    title { "Title" }
-    body { "body" }
+    title { 'Title' }
+    body { 'body' }
 
     trait :invalid do
       title { nil }
