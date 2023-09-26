4803d4cc35bafb91355211fd4fdd205b5a3c7512
pejath <pejath@yandex.ru>
2023-09-26 19:08:07 +0300

    rubocop run

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AnswersController < ApplicationController
   expose :question
   expose :answer
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,18 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  describe "POST #create" do
+  describe 'POST #create' do
     subject(:http_request) { post :create, params: params }
     let(:question) { create(:question) }
 
-    context "with valid attributes" do
+    context 'with valid attributes' do
       let(:params) { { question_id: question, answer: attributes_for(:answer, question: question) } }
 
-      it "saves a new answer in the database" do
+      it 'saves a new answer in the database' do
         expect { http_request }.to change(Answer, :count).by(1)
       end
 
-      it "redirects to associated question" do
+      it 'redirects to associated question' do
         expect(http_request).to redirect_to question
       end
 
@@ -24,17 +24,16 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
 
-    context "with invalid attributes" do
+    context 'with invalid attributes' do
       let(:params) { { question_id: question, answer: attributes_for(:answer, :invalid, question: question) } }
 
       it "doesn't save a new question in the database" do
         expect { http_request }.not_to change(Answer, :count)
       end
 
-      it "re-render new" do
+      it 're-render new' do
         expect(http_request).to render_template :new
       end
     end
   end
-
 end
