bb135ba811c60c51171c1a9e23a01a771fbacb2f
pejath <pejath@yandex.ru>
2023-10-09 17:18:40 +0300

    add links to answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -28,6 +28,6 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:answer).permit(:body, files: [])
+    params.require(:answer).permit(:body, files: [], links_attributes: [:name, :url, :_destroy])
   end
 end
---
+++ spec/features/answer/add_links_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'User can add links to question', %q{
+  In order to provide additional info to my question
+  As an question's author
+  I'd like to be able to add links
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:gist_url) { 'https://gist.github.com' }
+
+  scenario 'User adds link when adds answer' do
+    sign_in(user)
+    visit question_path(:question)
+
+    fill_in 'answer[body]', with: 'Test question'
+
+    fill_in 'Link name', with: 'Answer gist'
+    fill_in 'Url', with: gist_url
+
+    click_on 'Answer'
+
+    expect(page).to have_link 'Answer gist', href: gist_url
+  end
+end
