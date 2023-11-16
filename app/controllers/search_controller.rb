c24c8adfb61975cb3d4d1d8cdfe58755aa352cb4
pejath <pejath@yandex.ru>
2023-11-16 16:35:28 +0300

    sphinx

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,8 @@ class SearchController < ApplicationController
   def index
     @search_query = params[:query]
     @filters = params[:filters].try(:[], :filter)&.reject(&:empty?)
-
+    Rails.logger.info "Sphinx res: #{ThinkingSphinx.search @search_query, classes: @filters&.map(&:constantize)}"
+    
     @result = ThinkingSphinx.search @search_query, classes: @filters&.map(&:constantize)
   end
 end
